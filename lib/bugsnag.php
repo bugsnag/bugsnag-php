8ee846a634a4d511f1704a9eee71c67ba5bd9524
James Smith <james@loopj.com>
2013-01-28 15:39:11 -0800

    Fix PHP 5.2 issues with

--- lib/bugsnag.php
+++ lib/bugsnag.php
@@ -18,27 +18,29 @@
 class Bugsnag {
     // "Constants"
     private static $NOTIFIER = array(
-        'name' => 'Bugsnag PHP (Official)',
-        'version' => '1.0.3',
-        'url' => 'https://bugsnag.com'
+        'name'      => 'Bugsnag PHP (Official)',
+        'version'   => '1.0.3',
+        'url'       => 'https://bugsnag.com'
     );                                                                      
 
     private static $ERROR_NAMES = array (
-        \E_ERROR             => 'PHP Fatal Error',
-        \E_PARSE             => 'PHP Parse Error',
-        \E_COMPILE_ERROR     => 'PHP Compile Error',
-        \E_CORE_ERROR        => 'PHP Core Error',
-        \E_NOTICE            => 'PHP Notice',
-        \E_STRICT            => 'PHP Strict',
-        \E_DEPRECATED        => 'PHP Deprecated',
-        \E_WARNING           => 'PHP Warning',
-        \E_CORE_WARNING      => 'PHP Core Warning',
-        \E_COMPILE_WARNING   => 'PHP Compile Warning',
-        \E_RECOVERABLE_ERROR => 'PHP Recoverable Error',
-        \E_USER_ERROR        => 'User Error',
-        \E_USER_WARNING      => 'User Warning',
-        \E_USER_NOTICE       => 'User Notice',
-        \E_USER_DEPRECATED   => 'User Deprecated'
+        E_ERROR             => 'PHP Fatal Error',
+        E_PARSE             => 'PHP Parse Error',
+        E_COMPILE_ERROR     => 'PHP Compile Error',
+        E_CORE_ERROR        => 'PHP Core Error',
+        E_NOTICE            => 'PHP Notice',
+        E_STRICT            => 'PHP Strict',
+        E_WARNING           => 'PHP Warning',
+        E_CORE_WARNING      => 'PHP Core Warning',
+        E_COMPILE_WARNING   => 'PHP Compile Warning',
+        E_RECOVERABLE_ERROR => 'PHP Recoverable Error',
+        E_USER_ERROR        => 'User Error',
+        E_USER_WARNING      => 'User Warning',
+        E_USER_NOTICE       => 'User Notice',
+
+        // PHP 5.2 compatibility
+        8192                => 'PHP Deprecated',
+        16384               => 'User Deprecated'
     );
 
     private static $FATAL_ERRORS = array(
