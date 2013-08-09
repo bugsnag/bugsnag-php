07438831eca4becabe7ec9714f90893b3fabb2b9
James Smith <james@loopj.com>
2013-08-08 22:32:04 -0700

    Don't stub toArray

--- tests/Bugsnag/NotificationTest.php
+++ tests/Bugsnag/NotificationTest.php
@@ -11,15 +11,15 @@ class NotificationTest extends PHPUnit_Framework_TestCase {
     public function testNotification() {
         // Create a mock notification object
         $notification = $this->getMockBuilder('Bugsnag\Notification')
-                             ->setMethods(array("toArray"))
+                             ->setMethods(array("postJSON"))
                              ->setConstructorArgs(array($this->config))
                              ->getMock();
 
-        // Expect toArray to be called
+        // Expect postJSON to be called
         $notification->expects($this->once())
-                     ->method("toArray");
-
-        // TODO: Check toArray return value
+                     ->method("postJSON")
+                     ->with($this->anything(), 
+                            $this->anything());
 
         // Add an error to the notification and deliver it
         $notification->addError(Bugsnag\Error::fromNamedError($this->config, "Name", "Message"));
