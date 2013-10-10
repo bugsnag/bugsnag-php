c52172ac564cc8c6883069700c09e15dd71fae58
James Smith <james@loopj.com>
2013-10-10 13:49:53 -0700

    Update phar building

--- pharbuilder.php
+++ pharbuilder.php
@@ -1,7 +1,11 @@
 <?php
 
-$phar = new Phar('build/bugsnag.phar');
-$phar->buildFromDirectory(dirname(__FILE__) . '/src','/\.php$/');
-$phar->compressFiles(Phar::GZ);
-$phar->stopBuffering();
-$phar->setStub($phar->createDefaultStub('Bugsnag/Autoload.php'));
+function build_phar() {
+    $phar = new Phar('build/bugsnag.phar');
+    $phar->buildFromDirectory(dirname(__FILE__) . '/src','/\.php$/');
+    $phar->compressFiles(Phar::GZ);
+    $phar->stopBuffering();
+    $phar->setStub($phar->createDefaultStub('Bugsnag/Autoload.php'));
+}
+
+build_phar();
