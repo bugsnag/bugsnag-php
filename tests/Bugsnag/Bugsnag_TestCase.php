d2469570d1955a40e47010501ca68394648fe4e6
James Smith <james@loopj.com>
2013-12-18 11:23:44 -0800

    Use static factories to create Bugsnag_Error objects to ensure they are always complete, and to avoid unnecessary stacktrace generation. Fixes bugsnag/bugsnag-laravel#6

--- tests/Bugsnag/Bugsnag_TestCase.php
+++ tests/Bugsnag/Bugsnag_TestCase.php
@@ -2,8 +2,6 @@
 
 abstract class Bugsnag_TestCase extends PHPUnit_Framework_TestCase {
     protected function getError($name="Name", $message="Message") {
-        $error = new Bugsnag_Error($this->config, $this->diagnostics);
-        $error->setName($name)->setMessage($message);
-        return $error;
+        return Bugsnag_Error::fromNamedError($this->config, $this->diagnostics, $name, $message);
     }
 }
