a03d89899a39d0155f8d4fc8b78632ebcb09eb96
Graham Campbell <graham@bugsnag.com>
2016-06-21 17:00:18 +0100

    Fixed phpdoc

--- src/Bugsnag/Client.php
+++ src/Bugsnag/Client.php
@@ -226,8 +226,7 @@ class Bugsnag_Client
     }
 
     /**
-     * Set information about the current user of your app, including
-     * id, name and email.
+     * Set information about the current user of your app, including id, name and email.
      *
      * @param array $user an array of user information. Eg:
      *        array(
