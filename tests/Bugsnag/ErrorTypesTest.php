532cc8a934f728cc2ce1be05f88724cdfe0127f9
Graham Campbell <graham@bugsnag.com>
2016-06-23 11:43:52 +0100

    Added more error type tests

--- tests/Bugsnag/ErrorTypesTest.php
+++ tests/Bugsnag/ErrorTypesTest.php
@@ -8,4 +8,22 @@ class ErrorTypesTest extends PHPUnit_Framework_TestCase
         $this->assertSame(Bugsnag_ErrorTypes::getLevelsForSeverity('warning'), 674);
         $this->assertSame(Bugsnag_ErrorTypes::getLevelsForSeverity('info'), 27656);
     }
+
+    public function testIsFatal()
+    {
+        $this->assertFalse(Bugsnag_ErrorTypes::isFatal(E_CORE_WARNING));
+        $this->assertTrue(Bugsnag_ErrorTypes::isFatal(E_COMPILE_ERROR));
+    }
+
+    public function testGetName()
+    {
+        $this->assertSame('PHP Notice', Bugsnag_ErrorTypes::getName(E_NOTICE));
+        $this->assertSame('Unknown', Bugsnag_ErrorTypes::getName(42));
+    }
+
+    public function testGetSeverity()
+    {
+        $this->assertSame('info', Bugsnag_ErrorTypes::getSeverity(E_NOTICE));
+        $this->assertSame('error', Bugsnag_ErrorTypes::getSeverity(42));
+    }
 }
