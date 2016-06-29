e5e44362df85b90f4053dab88f1decaa334d0724
Graham Campbell <graham@alt-three.com>
2016-06-29 15:25:39 +0100

    Fixed typo

--- example/php/index.php
+++ example/php/index.php
@@ -2,5 +2,5 @@
 
 require_once 'vendor/autoload.php';
 
-$bugsnag = new Bugsnag\Client::make('YOUR-API-KEY-HERE');
+$bugsnag = Bugsnag\Client::make('YOUR-API-KEY-HERE');
 $bugsnag->notifyError('Broken', 'Something broke', ['tab' => ['paying' => true, 'object' => (object) ['key' => 'value'], 'null' => null, 'string' => 'test', 'int' => 4]]);
