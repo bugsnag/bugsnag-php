91d4ae6be16c204cb4b4e31cd537dc27fcde45d3
Graham Campbell <graham@alt-three.com>
2016-07-07 14:10:52 -0700

    Fixed the example (#291)

--- example/php/index.php
+++ example/php/index.php
@@ -3,4 +3,6 @@
 require_once 'vendor/autoload.php';
 
 $bugsnag = Bugsnag\Client::make('YOUR-API-KEY-HERE');
-$bugsnag->notifyError('Broken', 'Something broke', ['tab' => ['paying' => true, 'object' => (object) ['key' => 'value'], 'null' => null, 'string' => 'test', 'int' => 4]]);
+$bugsnag->notifyError('Broken', 'Something broke', function (Bugsnag\Report $report) {
+    $report->setMetaData(['tab' => ['paying' => true, 'object' => (object) ['key' => 'value'], 'null' => null, 'string' => 'test', 'int' => 4]]);
+});
