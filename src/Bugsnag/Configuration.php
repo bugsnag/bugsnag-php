fd8589564f9b6ceab2f4f8a1aad9ea26609931f9
Graham Campbell <graham@alt-three.com>
2016-07-06 18:15:43 -0700

    Release v2.9.1

--- src/Bugsnag/Configuration.php
+++ src/Bugsnag/Configuration.php
@@ -18,7 +18,7 @@ class Bugsnag_Configuration
     public $proxySettings = array();
     public $notifier = array(
         'name' => 'Bugsnag PHP (Official)',
-        'version' => '2.9.0',
+        'version' => '2.9.1',
         'url' => 'https://bugsnag.com',
     );
     public $sendEnvironment = false;
