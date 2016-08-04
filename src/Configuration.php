f401f217d38165201d1309f3bd00a2677f4c776d
Graham Campbell <graham@bugsnag.com>
2016-08-04 10:39:08 +0100

    Target 3.2

--- src/Configuration.php
+++ src/Configuration.php
@@ -62,7 +62,7 @@ class Configuration
      */
     protected $notifier = [
         'name' => 'Bugsnag PHP (Official)',
-        'version' => '3.1.1',
+        'version' => '3.2.0',
         'url' => 'https://bugsnag.com',
     ];
 
