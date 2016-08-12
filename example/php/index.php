85d6efe841da23014226bd7ad4419d037937cb65
Graham Campbell <graham@alt-three.com>
2016-08-12 23:42:51 +0100

    Made the example leave a breadcrumb (#350)

--- example/php/index.php
+++ example/php/index.php
@@ -3,6 +3,9 @@
 require_once 'vendor/autoload.php';
 
 $bugsnag = Bugsnag\Client::make('YOUR-API-KEY-HERE');
+
+$bugsnag->leaveBreadcrumb('Example breadcrumb!');
+
 $bugsnag->notifyError('Broken', 'Something broke', function (Bugsnag\Report $report) {
     $report->setMetaData(['tab' => ['paying' => true, 'object' => (object) ['key' => 'value'], 'null' => null, 'string' => 'test', 'int' => 4]]);
 });
