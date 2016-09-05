2d74d2404199ae6dcd4cd97c284614028898faea
Graham Campbell <graham@bugsnag.com>
2016-09-05 09:22:19 +0100

    Test getting app data behaves correctly

--- tests/ConfigurationTest.php
+++ tests/ConfigurationTest.php
@@ -70,11 +70,19 @@ class ConfigurationTest extends TestCase
 
         $this->assertSame(['type' => 'cli', 'releaseStage' => 'qa1', 'version' => '1.2.3'], $this->config->getAppData());
 
+        $this->config->setFallbackType('foo');
+
+        $this->assertSame(['type' => 'foo', 'releaseStage' => 'qa1', 'version' => '1.2.3'], $this->config->getAppData());
+
         $this->config->setReleaseStage(null);
 
-        $this->assertSame(['type' => 'cli', 'releaseStage' => 'production', 'version' => '1.2.3'], $this->config->getAppData());
+        $this->assertSame(['type' => 'foo', 'releaseStage' => 'production', 'version' => '1.2.3'], $this->config->getAppData());
 
         $this->config->setAppVersion(null);
+
+        $this->assertSame(['type' => 'foo', 'releaseStage' => 'production'], $this->config->getAppData());
+
+        $this->config->setFallbackType(null);
 
         $this->assertSame(['type' => 'cli', 'releaseStage' => 'production'], $this->config->getAppData());
     }
