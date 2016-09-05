c3b05a18f9116ac7cb4984fd7ee0614fe4490176
Graham Campbell <graham@alt-three.com>
2016-09-05 16:23:22 +0100

    Release 3.4.0 (#365)

--- src/Configuration.php
+++ src/Configuration.php
@@ -62,7 +62,7 @@ class Configuration
      */
     protected $notifier = [
         'name' => 'Bugsnag PHP (Official)',
-        'version' => '3.3.0',
+        'version' => '3.4.0',
         'url' => 'https://bugsnag.com',
     ];
 
