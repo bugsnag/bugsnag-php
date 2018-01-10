f26f6a10877a0168c597954626cf9a347fdb985c
Delisa Mason <kattrali@users.noreply.github.com>
2018-01-10 10:13:59 -0800

    Apply fixes from StyleCI (#455)

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -71,6 +71,7 @@ class HttpClient
 
     /**
      * Notify Bugsnag of a deployment.
+     *
      * @deprecated This method should no longer be used in favour of sendBuildReport.
      *
      * @param array $data the deployment information
