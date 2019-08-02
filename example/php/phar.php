657d0f01693a3f9894f4b54ce64e1474e4f81165
Alex Moinet <alex@bugsnag.com>
2019-08-02 16:08:06 +0100

    Apply fixes from StyleCI (#534)

--- example/php/phar.php
+++ example/php/phar.php
@@ -2,8 +2,8 @@
 
 // Includes Bugsnag from a downloaded php archive file.  These should be located
 // in the same folder as this script.  See the Readme for more information.
-include "phar://bugsnag.phar";
-include "phar://guzzle.phar";
+include 'phar://bugsnag.phar';
+include 'phar://guzzle.phar';
 
 // Create the bugsnag client
 $bugsnag = Bugsnag\Client::make();
