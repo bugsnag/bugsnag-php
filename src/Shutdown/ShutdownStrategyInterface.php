4dd3b90f18f7ed3e986e38f0c57d0a1550a6cfc3
Delisa <iskanamagus@gmail.com>
2019-10-29 09:31:20 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Shutdown/ShutdownStrategyInterface.php
+++ src/Shutdown/ShutdownStrategyInterface.php
@@ -13,6 +13,7 @@ interface ShutdownStrategyInterface
      * Register the shutdown behaviour.
      *
      * @param \Bugsnag\Client $client
+     *
      * @return void
      */
     public function registerShutdownStrategy(Client $client);
