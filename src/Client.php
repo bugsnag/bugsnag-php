5f6fa23d33458deb6e4a632ec1051c5121d7cf62
Richard Harrison <richard@bugsnag.com>
2019-11-04 17:24:03 +0000

    Replaced the dependency on phpdotenv with a simple read function that follows the same strategy

--- src/Client.php
+++ src/Client.php
@@ -17,7 +17,6 @@ use Bugsnag\Middleware\SessionData;
 use Bugsnag\Request\BasicResolver;
 use Bugsnag\Request\ResolverInterface;
 use Composer\CaBundle\CaBundle;
-use Dotenv\Environment\DotenvFactory;
 use GuzzleHttp\Client as Guzzle;
 use GuzzleHttp\ClientInterface;
 use ReflectionClass;
@@ -87,7 +86,8 @@ class Client
      */
     public static function make($apiKey = null, $endpoint = null, $defaults = true)
     {
-        $env = (new DotenvFactory())->create();
+        // Retrieves environment variables
+        $env = new Env();
 
         $config = new Configuration($apiKey ?: $env->get('BUGSNAG_API_KEY'));
         $guzzle = static::makeGuzzle($endpoint ?: $env->get('BUGSNAG_ENDPOINT'));
---
+++ src/Env.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Bugsnag;
+
+class Env
+{
+    /**
+     * Reads an environment variable from $_ENV, $_SERVER or via getenv(). Supports a thread-safe read via the
+     * superglobals, but falls back on getenv() to allow for other methods of setting environment data. See this article
+     * for more background context: https://mattallan.me/posts/how-php-environment-variables-actually-work/
+     *
+     * Copied from phpdotenv: https://github.com/vlucas/phpdotenv/blob/2.6/src/Loader.php#L291
+     *
+     * @param $name
+     *
+     * @return array|false|mixed|string|null
+     */
+    public function get($name)
+    {
+        switch (true) {
+            case array_key_exists($name, $_ENV):
+                return $_ENV[$name];
+
+            case array_key_exists($name, $_SERVER):
+                return $_SERVER[$name];
+
+            default:
+                $value = getenv($name);
+                return $value === false ? null : $value; // switch getenv default to null
+        }
+    }
+}
