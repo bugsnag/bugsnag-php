20ad83a27bfcf92847e536dad3f68e9e33563e29
Alex Moinet <Alex.moinet@yahoo.co.uk>
2020-01-03 17:40:38 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- example/code-igniter-3.1/application/views/errors/html/error_exception.php
+++ example/code-igniter-3.1/application/views/errors/html/error_exception.php
@@ -11,22 +11,22 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <p>Filename: <?php echo $exception->getFile(); ?></p>
 <p>Line Number: <?php echo $exception->getLine(); ?></p>
 
-<?php if (defined('SHOW_DEBUG_BACKTRACE') && SHOW_DEBUG_BACKTRACE === true): ?>
+<?php if (defined('SHOW_DEBUG_BACKTRACE') && SHOW_DEBUG_BACKTRACE === true) { ?>
 
 	<p>Backtrace:</p>
-	<?php foreach ($exception->getTrace() as $error): ?>
+	<?php foreach ($exception->getTrace() as $error) { ?>
 
-		<?php if (isset($error['file']) && strpos($error['file'], realpath(BASEPATH)) !== 0): ?>
+		<?php if (isset($error['file']) && strpos($error['file'], realpath(BASEPATH)) !== 0) { ?>
 
 			<p style="margin-left:10px">
 			File: <?php echo $error['file']; ?><br />
 			Line: <?php echo $error['line']; ?><br />
 			Function: <?php echo $error['function']; ?>
 			</p>
-		<?php endif ?>
+		<?php } ?>
 
-	<?php endforeach ?>
+	<?php } ?>
 
-<?php endif ?>
+<?php } ?>
 
 </div>
