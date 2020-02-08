561afd4679d5077e64dfa97153e7a2e8b4971af4
Graham Campbell <graham@alt-three.com>
2020-02-08 18:46:09 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,8 +8,8 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use PHPMock, MockeryTrait;
-
+    use PHPMock;
+    use MockeryTrait;
     public function expectedException($class, $msg = null)
     {
         if (class_exists(\PHPUnit_Framework_TestCase::class)) {
