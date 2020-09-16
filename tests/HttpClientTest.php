3e248f21c22537d98eb704c1c2655cb0aba27061
Joe Haines <hello@joehaines.co.uk>
2020-09-16 16:18:04 +0100

    Rewrite HttpClientTest to avoid removed features
    This test was heavily using internal features of PHPUnit which
    have since been removed
    
    Refactored it to use regular mocks, which should be more resilient
    to change

--- tests/HttpClientTest.php
+++ tests/HttpClientTest.php
@@ -7,11 +7,23 @@ use Bugsnag\HttpClient;
 use Bugsnag\Report;
 use Exception;
 use GuzzleHttp\Client;
+use PHPUnit\Framework\MockObject\MockObject;
 
 class HttpClientTest extends TestCase
 {
+    /**
+     * @var Configuration
+     */
     protected $config;
+
+    /**
+     * @var MockObject&Client
+     */
     protected $guzzle;
+
+    /**
+     * @var HttpClient
+     */
     protected $http;
 
     /**
@@ -21,170 +33,177 @@ class HttpClientTest extends TestCase
     {
         $this->config = new Configuration('6015a72ff14038114c3d12623dfb018f');
 
+        /** @var MockObject&Client */
         $this->guzzle = $this->getMockBuilder(Client::class)
-                             ->setMethods([self::getGuzzleMethod()])
-                             ->getMock();
+            ->setMethods([self::getGuzzleMethod()])
+            ->getMock();
 
         $this->http = new HttpClient($this->config, $this->guzzle);
     }
 
-    private static function getInvocationParameters($invocation)
+    private function setExpectedGuzzleParameters($expectation, $callback)
     {
-        if (is_callable([$invocation, 'getParameters'])) {
-            return $invocation->getParameters();
+        if ($this->isUsingGuzzle5()) {
+            $expectation->with(
+                $this->config->getNotifyEndpoint(),
+                $this->callback($callback)
+            );
+        } else {
+            $expectation->with(
+                'POST',
+                $this->config->getNotifyEndpoint(),
+                $this->callback($callback)
+            );
         }
-
-        return $invocation->parameters;
     }
 
     public function testHttpClient()
     {
-        // Expect request to be called
-        $this->guzzle->expects($spy = $this->any())->method(self::getGuzzleMethod());
+        $verifyGuzzleParameters = function ($options) {
+            Assert::isType('array', $options);
+            Assert::isType('array', $options['json']['notifier']);
+            Assert::isType('array', $options['json']['events']);
+            $this->assertSame([], $options['json']['events'][0]['user']);
+            $this->assertSame(['foo' => 'bar'], $options['json']['events'][0]['metaData']);
+            $this->assertSame('6015a72ff14038114c3d12623dfb018f', $options['json']['apiKey']);
+            $this->assertSame('4.0', $options['json']['events'][0]['payloadVersion']);
 
-        // Add a report to the http and deliver it
+            $headers = $options['headers'];
+
+            $this->assertSame('6015a72ff14038114c3d12623dfb018f', $headers['Bugsnag-Api-Key']);
+            $this->assertArrayHasKey('Bugsnag-Sent-At', $headers);
+            $this->assertSame('4.0', $headers['Bugsnag-Payload-Version']);
+
+            return true;
+        };
+
+        $expectation = $this->guzzle->expects($this->once())
+            ->method(self::getGuzzleMethod());
+
+        $this->setExpectedGuzzleParameters($expectation, $verifyGuzzleParameters);
+
         $this->http->queue(Report::fromNamedError($this->config, 'Name')->setMetaData(['foo' => 'bar']));
         $this->http->send();
-
-        $this->assertCount(1, $invocations = $spy->getInvocations());
-        $params = self::getInvocationParameters($invocations[0]);
-        $this->assertCount(self::getGuzzleExpectedParamCount(), $params);
-        $this->assertSame($this->config->getNotifyEndpoint(), self::getGuzzlePostUriParam($params));
-        $options = self::getGuzzlePostOptionsParam($params);
-        Assert::isType('array', $options);
-        Assert::isType('array', $options['json']['notifier']);
-        Assert::isType('array', $options['json']['events']);
-        $this->assertSame([], $options['json']['events'][0]['user']);
-        $this->assertSame(['foo' => 'bar'], $options['json']['events'][0]['metaData']);
-        $this->assertSame('6015a72ff14038114c3d12623dfb018f', $options['json']['apiKey']);
-        $this->assertSame('4.0', $options['json']['events'][0]['payloadVersion']);
-
-        $headers = $options['headers'];
-        $this->assertSame('6015a72ff14038114c3d12623dfb018f', $headers['Bugsnag-Api-Key']);
-        $this->assertArrayHasKey('Bugsnag-Sent-At', $headers);
-        $this->assertSame('4.0', $headers['Bugsnag-Payload-Version']);
     }
 
     public function testHttpClientMultipleSend()
     {
-        // Expect request to be called
-        $this->guzzle->expects($spy = $this->any())->method(self::getGuzzleMethod());
+        $verifyGuzzleParameters = function ($options) {
+            Assert::isType('array', $options);
+            Assert::isType('array', $options['json']['notifier']);
+            Assert::isType('array', $options['json']['events']);
+            $this->assertSame([], $options['json']['events'][0]['user']);
+            $this->assertSame(['foo' => 'bar'], $options['json']['events'][0]['metaData']);
+            $this->assertSame('6015a72ff14038114c3d12623dfb018f', $options['json']['apiKey']);
+            $this->assertSame('4.0', $options['json']['events'][0]['payloadVersion']);
 
-        // Add a report to the http and deliver it
+            $headers = $options['headers'];
+
+            $this->assertSame('6015a72ff14038114c3d12623dfb018f', $headers['Bugsnag-Api-Key']);
+            $this->assertArrayHasKey('Bugsnag-Sent-At', $headers);
+            $this->assertSame('4.0', $headers['Bugsnag-Payload-Version']);
+
+            return true;
+        };
+
+        $expectation = $this->guzzle->expects($this->once())
+            ->method(self::getGuzzleMethod());
+
+        $this->setExpectedGuzzleParameters($expectation, $verifyGuzzleParameters);
+
         $this->http->queue(Report::fromNamedError($this->config, 'Name')->setMetaData(['foo' => 'bar']));
         $this->http->send();
-
-        // Make sure these do nothing
         $this->http->send();
-        $this->http->send();
-
-        // Check we only sent once
-        $this->assertCount(1, $invocations = $spy->getInvocations());
     }
 
     public function testMassiveMetaDataHttpClient()
     {
-        // Expect request to be called
-        $this->guzzle->expects($spy = $this->any())->method(self::getGuzzleMethod());
+        $verifyGuzzleParameters = function ($options) {
+            Assert::isType('array', $options);
+            Assert::isType('array', $options['json']['notifier']);
+            Assert::isType('array', $options['json']['events']);
+            $this->assertSame([], $options['json']['events'][0]['user']);
+            $this->assertArrayNotHasKey('metaData', $options['json']['events'][0]);
+            $this->assertSame('6015a72ff14038114c3d12623dfb018f', $options['json']['apiKey']);
+            $this->assertSame('4.0', $options['json']['events'][0]['payloadVersion']);
 
-        // Add a report to the http and deliver it
+            $headers = $options['headers'];
+
+            $this->assertSame('6015a72ff14038114c3d12623dfb018f', $headers['Bugsnag-Api-Key']);
+            $this->assertArrayHasKey('Bugsnag-Sent-At', $headers);
+            $this->assertSame('4.0', $headers['Bugsnag-Payload-Version']);
+
+            return true;
+        };
+
+        $expectation = $this->guzzle->expects($this->once())
+            ->method(self::getGuzzleMethod());
+
+        $this->setExpectedGuzzleParameters($expectation, $verifyGuzzleParameters);
+
         $this->http->queue(Report::fromNamedError($this->config, 'Name')->setMetaData(['foo' => str_repeat('A', 1500000)]));
         $this->http->send();
-
-        $this->assertCount(1, $invocations = $spy->getInvocations());
-        $params = self::getInvocationParameters($invocations[0]);
-        $this->assertCount(self::getGuzzleExpectedParamCount(), $params);
-        $this->assertSame($this->config->getNotifyEndpoint(), self::getGuzzlePostUriParam($params));
-        $options = self::getGuzzlePostOptionsParam($params);
-        Assert::isType('array', $options);
-        Assert::isType('array', $options['json']['notifier']);
-        Assert::isType('array', $options['json']['events']);
-        $this->assertSame([], $options['json']['events'][0]['user']);
-        $this->assertArrayNotHasKey('metaData', $options['json']['events'][0]);
-        $this->assertSame('6015a72ff14038114c3d12623dfb018f', $options['json']['apiKey']);
-        $this->assertSame('4.0', $options['json']['events'][0]['payloadVersion']);
-
-        $headers = $options['headers'];
-        $this->assertSame('6015a72ff14038114c3d12623dfb018f', $headers['Bugsnag-Api-Key']);
-        $this->assertArrayHasKey('Bugsnag-Sent-At', $headers);
-        $this->assertSame('4.0', $headers['Bugsnag-Payload-Version']);
     }
 
     public function testMassiveUserHttpClient()
     {
-        // Setup error_log mocking
         $log = $this->getFunctionMock('Bugsnag', 'error_log');
-        $log->expects($this->once())->with($this->equalTo('Bugsnag Warning: Payload too large'));
+        $log->expects($this->once())
+            ->with($this->equalTo('Bugsnag Warning: Payload too large'));
 
-        // Expect request to be called
-        $this->guzzle->expects($spy = $this->any())->method(self::getGuzzleMethod());
+        $this->guzzle->expects($this->never())
+            ->method(self::getGuzzleMethod())
+            ->withAnyParameters();
 
-        // Add a report to the http and deliver it
         $this->http->queue(Report::fromNamedError($this->config, 'Name')->setUser(['foo' => str_repeat('A', 1500000)]));
         $this->http->send();
-
-        $this->assertCount(0, $spy->getInvocations());
     }
 
     public function testPartialHttpClient()
     {
-        // Setup error_log mocking
         $log = $this->getFunctionMock('Bugsnag', 'error_log');
         $log->expects($this->once())->with($this->equalTo('Bugsnag Warning: Payload too large'));
 
-        // Expect request to be called
-        $this->guzzle->expects($spy = $this->any())->method(self::getGuzzleMethod());
+        $verifyGuzzleParameters = function ($options) {
+            Assert::isType('array', $options);
+            Assert::isType('array', $options['json']['notifier']);
+            Assert::isType('array', $options['json']['events']);
+            $this->assertSame(['foo' => 'bar'], $options['json']['events'][0]['user']);
+            $this->assertSame([], $options['json']['events'][0]['metaData']);
+            $this->assertSame('6015a72ff14038114c3d12623dfb018f', $options['json']['apiKey']);
+            $this->assertSame('4.0', $options['json']['events'][0]['payloadVersion']);
 
-        // Add two errors to the http and deliver them
+            $headers = $options['headers'];
+            $this->assertSame('6015a72ff14038114c3d12623dfb018f', $headers['Bugsnag-Api-Key']);
+            $this->assertArrayHasKey('Bugsnag-Sent-At', $headers);
+            $this->assertSame('4.0', $headers['Bugsnag-Payload-Version']);
+
+            return true;
+        };
+
+        $expectation = $this->guzzle->expects($this->once())
+            ->method(self::getGuzzleMethod());
+
+        $this->setExpectedGuzzleParameters($expectation, $verifyGuzzleParameters);
+
         $this->http->queue(Report::fromNamedError($this->config, 'Name')->setUser(['foo' => str_repeat('A', 1500000)]));
         $this->http->queue(Report::fromNamedError($this->config, 'Name')->setUser(['foo' => 'bar']));
         $this->http->send();
-
-        $this->assertCount(1, $invocations = $spy->getInvocations());
-        $params = self::getInvocationParameters($invocations[0]);
-        $this->assertCount(self::getGuzzleExpectedParamCount(), $params);
-        $this->assertSame($this->config->getNotifyEndpoint(), self::getGuzzlePostUriParam($params));
-        $options = self::getGuzzlePostOptionsParam($params);
-        Assert::isType('array', $options);
-        Assert::isType('array', $options['json']['notifier']);
-        Assert::isType('array', $options['json']['events']);
-        $this->assertSame(['foo' => 'bar'], $options['json']['events'][0]['user']);
-        $this->assertSame([], $options['json']['events'][0]['metaData']);
-        $this->assertSame('6015a72ff14038114c3d12623dfb018f', $options['json']['apiKey']);
-        $this->assertSame('4.0', $options['json']['events'][0]['payloadVersion']);
-
-        $headers = $options['headers'];
-        $this->assertSame('6015a72ff14038114c3d12623dfb018f', $headers['Bugsnag-Api-Key']);
-        $this->assertArrayHasKey('Bugsnag-Sent-At', $headers);
-        $this->assertSame('4.0', $headers['Bugsnag-Payload-Version']);
     }
 
     public function testHttpClientFails()
     {
-        // Setup error_log mocking
         $log = $this->getFunctionMock('Bugsnag', 'error_log');
-        $log->expects($this->once())->with($this->equalTo('Bugsnag Warning: Couldn\'t notify. Guzzle exception thrown!'));
+        $log->expects($this->once())
+            ->with($this->equalTo(
+                "Bugsnag Warning: Couldn't notify. Guzzle exception thrown!"
+            ));
 
-        // Expect request to be called
-        $this->guzzle->method(self::getGuzzleMethod())->will($this->throwException(new Exception('Guzzle exception thrown!')));
+        $this->guzzle->expects($this->once())
+            ->method(self::getGuzzleMethod())
+            ->will($this->throwException(new Exception('Guzzle exception thrown!')));
 
-        // Add a report to the http and deliver it
         $this->http->queue(Report::fromNamedError($this->config, 'Name')->setMetaData(['foo' => 'bar']));
         $this->http->send();
-    }
-
-    private function getGuzzleExpectedParamCount()
-    {
-        return self::getGuzzleMethod() === 'request' ? 3 : 2;
-    }
-
-    private function getGuzzlePostUriParam(array $params)
-    {
-        return $params[self::getGuzzleMethod() === 'request' ? 1 : 0];
-    }
-
-    private function getGuzzlePostOptionsParam(array $params)
-    {
-        return $params[self::getGuzzleMethod() === 'request' ? 2 : 1];
     }
 }
