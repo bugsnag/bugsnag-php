e68a0e179f486c3fe349278b6216681f96cd5407
Joe Haines <hello@joehaines.co.uk>
2020-10-23 17:53:42 +0100

    Use new Date class

--- tests/Breadcrumbs/BreadcrumbTest.php
+++ tests/Breadcrumbs/BreadcrumbTest.php
@@ -66,7 +66,7 @@ class BreadcrumbTest extends TestCase
 
         Assert::isType('array', $breadcrumb->toArray());
         $this->assertCount(3, $breadcrumb->toArray());
-        Assert::matchesDateFormat('Y-m-d\TH:i:s\Z', $breadcrumb->toArray()['timestamp']);
+        Assert::matchesBugsnagDateFormat($breadcrumb->toArray()['timestamp']);
         $this->assertSame('Foo', $breadcrumb->toArray()['name']);
         $this->assertSame('request', $breadcrumb->toArray()['type']);
     }
